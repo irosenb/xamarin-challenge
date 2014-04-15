8a92b3e2d686bd61869b200157d3a7c94d1b7bc1
Isaac Rosenberg <irosenb7@gmail.com>
2014-04-15 12:50:53 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XamarinChallenge::Application.config.secret_key_base = 'd053c075591841612b7880f9f1ce8ad7893da2c6f46cb476e95511844b6b1425653b7e3c7dc619224205bf962c6eccfe386ee79a991a67ac15dbf6c85c066e06'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+XamarinChallenge::Application.config.session_store :cookie_store, key: '_xamarin-challenge_session'
